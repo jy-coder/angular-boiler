c089fd071abbbe3cd572d85422def85bde0ed867
jy-coder <cjyclx@gmail.com>
2023-06-25 15:47:47 +0800

    [UPDATE] Add create options to option model file

--- src/app/models/options.ts
+++ src/app/models/options.ts
@@ -1,7 +1,16 @@
+import { Category } from './category';
+
 export interface CategoryOptions {
   id: number;
   name: string;
   isSelected: boolean;
 }
 
+export function createCategoryOption(category: Category, event: Event): CategoryOptions {
+  return {
+    id: category.id || 0,
+    name: category.name,
+    isSelected: (event.target as HTMLInputElement).checked,
+  };
+}
 export type Option = CategoryOptions;
